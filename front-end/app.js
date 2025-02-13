318af3bf0b01e511296713686b656903eea38129
Gromuf <luigi.beaumois@gmail.com>
2025-02-13 10:03:13 +0100

    page d'accueil (simple html css js)

---
+++ front-end/app.js
@@ -0,0 +1,10 @@
+// Gestion des boutons de navigation
+document.getElementById("registerBtn").addEventListener("click", () => {
+  window.location.href = "register.html";
+});
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  window.location.href = "login.html";
+});
+
+console.log("🏡 Interface d'accueil chargée avec succès.");
