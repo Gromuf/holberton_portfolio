b3c43bd727b5ad9e56dcdacedf476ae50bdafe73
Gromuf <luigi.beaumois@gmail.com>
2025-02-17 15:43:09 +0100

    aaaaa

--- snake-game-backend/src/main/java/com/mygame/App.java
+++ snake-game-backend/src/main/java/com/mygame/App.java
@@ -3,7 +3,7 @@ package com.mygame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mygame")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
